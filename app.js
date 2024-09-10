e028cc36a8daf67379d5863739f0881a0e3f292e
Bijo Murmu <bijomurmu036@gmail.com>
2024-09-10 10:29:09 +0530

    Fixing errors

--- app.js
+++ app.js
@@ -91,8 +91,8 @@ app.all("*", (req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  let { statusCode = 500, message = "Something went wrong!" } = err;
-  console.error("Error stack:", err.stack); // Log the error stack
+  let { statusCode = 500, message} = err;
+  console.error("Error stack:", err.stack); // Log the error stack // this line has been rendered. 
   res.status(statusCode).render("error.ejs", { message });
 });
 
